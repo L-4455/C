b84c5916cfa26516ad798650448aa926eee4133c
Jack <2382395063@qq.com>
2022-04-13 22:10:34 +0800

    Initial Commit.

---
+++ project/Test01.c
@@ -0,0 +1,14 @@
+/** 
+ * @author: LiZiHao
+ * @Date: 2022-04-13 15:59:29
+ * @LastEditTime: 2022-04-13 21:31:33
+ */
+#include <stdio.h>
+
+int main() {
+  printf("HelloWorld\n");
+  int num;
+  printf("请输入一个数字\n");
+  scanf("%d", &num);
+  return 0;
+}
---
+++ project/Test03.c
@@ -0,0 +1,10 @@
+/** 
+ * @author: LiZiHao
+ * @Date: 2022-04-13 22:04:44
+ * @LastEditTime: 2022-04-13 22:06:59
+ */
+#include<stdio.h>
+
+int main(){
+    printf("Œ“≤¡");
+}
