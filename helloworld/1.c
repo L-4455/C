a36b150f948cb1baaa32560c5396c852add618cf
Jack <l2382395063@gmail.com>
2022-05-04 23:37:49 +0800

    Merge branch 'master' of https://gitee.com/lcssvip/c_projects

---
+++ helloworld/1.c
@@ -0,0 +1,18 @@
+/**
+ * @author LiZiHao
+ * @date 2022-05-04
+ */
+
+#include <stdio.h>
+/**
+ * @brief
+ *
+ *
+ * @return int
+ */
+int main() {
+  int count = 100;
+  for (size_t i = 0; i < count; i++) {
+      printf("%d\n",i);
+  }
+}
