18107ac791e29746c1db51249203142d67700518
落尘 <l2382395063@gmail.com>
2022-06-08 17:01:02 +0800

    初始提交

--- Test01/System/Stdlib.c
+++ Test01/System/Stdlib.c
@@ -1,12 +1,7 @@
-/** 
- * @Date: 2022-05-18 15:38:18
- * @LastEditors: LiZiHao
- * @LastEditTime: 2022-05-23 22:37:43
- */
 /**
  * @Date: 2022-05-18 15:38:18
  * @LastEditors: LiZiHao
- * @LastEditTime: 2022-05-18 21:24:50
+ * @LastEditTime: 2022-06-08 17:00:31
  */
 
 #include <stdio.h>
@@ -22,5 +17,4 @@ int main() {
   for (int i = 0; i < 5; i++) {
     printf("%d\n", rand() % 50);
   }
-
 }
